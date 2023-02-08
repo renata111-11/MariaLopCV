a4c1db45c0a5d1c62dbb540a7fcc34a0e923fed6
renata111-11 <renatamariarojas@gmail.com>
2023-02-08 02:46:34 -0300

    se agrega el proyecto

---
+++ script.js
@@ -0,0 +1,33 @@
+
+let hideText_btn = document.getElementById('hideText_btn');
+
+let hideText = document.getElementById('hideText');
+
+hideText_btn.addEventListener('click', toggleText);
+
+function toggleText() {
+	hideText.classList.toggle('show');
+}
+
+
+let hideText_btn1 = document.getElementById('hideText_btn1');
+
+let hideText1 = document.getElementById('hideText1');
+
+hideText_btn1.addEventListener('click', toggleText1);
+
+function toggleText1() {
+	hideText1.classList.toggle('show');
+}
+
+let hideText_btn2 = document.getElementById('hideText_btn2');
+
+let hideText2 = document.getElementById('hideText2');
+
+hideText_btn2.addEventListener('click', toggleText2);
+
+function toggleText2() {
+	hideText2.classList.toggle('show');
+}
+
+
